4cfe053d2c7a225f55a0d0e3286a9a9a61d28b63
gyotro <giovanni.dintrono@gmail.com>
2023-04-03 00:01:03 +0200

    aggiunta navigation tra lista e dettaglio

--- app/src/main/java/com/example/gyotestapp/MainActivity.kt
+++ app/src/main/java/com/example/gyotestapp/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -20,9 +21,14 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import com.example.gyotestapp.ui.theme.GyoTestAppTheme
@@ -35,276 +41,310 @@ class MainActivity : ComponentActivity() {
         setContent(content = {
             // carichiamo il template custom
             GyoTestAppTheme() {
-                UserListScreen(userProfileList)
+                UsersApplicationNavigation()
             }
-
         })
     }
+}
 
-    @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
-    @Composable
-    fun UserListScreen(userProfile: List<UserProfile>) {
-        Scaffold(topBar = { AppBar() }) {
-            Surface(
-                color = Color.LightGray,
-                modifier = Modifier.fillMaxSize()
-            ) {
-                LazyColumn {
-                   items(userProfile) { userProfileItem ->
-                       ProfileCard(userProfileItem)
-                   }
-                }
-                /* Column {
-                        userProfile.forEach() {
-                            ProfileCard(it)
-                        }*/
-            }
-        }
-    }
-
-    @Composable
-    fun AppBar() {
-        TopAppBar(
-            navigationIcon = { Icon(Icons.Default.Person,
-                "Home",
-                modifier = Modifier.padding(12.dp))
-                             },
-            title = {Text("Messaging Application Users")}
-        )
-
-    }
-
-    @Composable
-    fun ProfileCard(userProfile: UserProfile) {
-        Card(
-            modifier = Modifier
-                .padding(top = 10.dp,
-                    start = 10.dp,
-                    bottom = 10.dp ,
-                    end = 10.dp)
-                .fillMaxWidth()
-                .wrapContentHeight(align = Alignment.Top),
-            elevation = 8.dp
-        ) {
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.Start
-            ) {
-                ProfilePicture(userProfile.drawableUrl, userProfile.status)
-                ProfileContent(userProfile.name, userProfile.status)
-            }
-        }
-    }
-
-    @Composable
-    fun ProfilePicture(drawableUrl: String, status: Boolean, imageSize: Int = 90){
-        Card(
-            shape = CircleShape,
-            border = BorderStroke(
-                width = 2.dp,
-                color = if (status)
-                    MaterialTheme.colors.lightGreen
-                else
-                    Color.Red
-        ),
-            modifier = Modifier.padding(16.dp),
-            elevation = 4.dp
-
-        ) {
-            AsyncImage(
-                model = ImageRequest.Builder(LocalContext.current)
-                    .data(drawableUrl)
-                    .crossfade(true)
-                    .build(),
-                placeholder = painterResource(R.drawable.error),
-                contentDescription = "image",
-                contentScale = ContentScale.Crop,
-                modifier = Modifier.clip(CircleShape)
-                    .size(imageSize.dp)
-            )
-        }
-    }
-
-    @Composable
-    fun ProfileContent(name: String, status: Boolean, alignment: Alignment.Horizontal = Alignment.Start) {
-        Column(
-            modifier = Modifier
-//   la commentiamo per evitare che sia mal allineato lo screen UserProfile
-//                .fillMaxWidth()
-                .padding(8.dp),
-            horizontalAlignment = alignment
-        ) {
-            Text(
-                text = name,
-                color = Color.DarkGray,
-                style = MaterialTheme.typography.h4,
-             //   textAlign = TextAlign.Center
-            )
-            Text(
-                text = if (status)
-                    "Active now"
-                else
-                    "Offline",
-                color = Color.DarkGray,
-                style = MaterialTheme.typography.h6,
-                //textAlign = TextAlign.Center
-            )
-        }
-    }
-
-    @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
-    @Composable
-    fun UserDetailsScreen(userProfile: UserProfile = userProfileList[0]) {
-        Scaffold(topBar = { AppBar() }) {
-            Surface(
-                color = Color.White,
-                modifier = Modifier.fillMaxSize()
-            )
-            {
-                Column(
-                    // modifier = androidx.compose.ui.Modifier.fillMaxWidth(),
-                    //HorizontalAlignment = androidx.compose.ui.Alignment.CenterVertically,
-                    //horizontalArrangement = androidx.compose.foundation.layout.Arrangement.Start
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.Top
-                ) {
-                    ProfilePicture(userProfile.drawableUrl, userProfile.status, imageSize = 250)
-                    ProfileContent(userProfile.name, userProfile.status, Alignment.CenterHorizontally)
-                }
-            }
-        }
-    }
-
-    /*
-    @Composable
-    fun MainScreen(viewModel: MainViewModel = MainViewModel()) {
-        val mutableList = viewModel.mutableLiveDataList.observeAsState(mutableListOf<String>(""))
-        val mutablelStateValue = viewModel.textField.observeAsState("")
-        Surface(color = Color.DarkGray,
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+@Composable
+fun UserListScreen(userProfile: List<UserProfile>, navController: NavHostController? = null) {
+    Scaffold(topBar = { AppBar() }) {
+        Surface(
+            color = Color.Transparent,
             modifier = Modifier.fillMaxSize()
         ) {
-            SetColumns( mutableList.value,
-                buttonClick = { viewModel.addValueToList(mutablelStateValue.value)
-                    viewModel.onTextChanged("")
-                     },
-                mutablelStateValue = mutablelStateValue.value
-            ) { // definizione della lambda di quando inseriamo testo
-                    input: String -> viewModel.onTextChanged(input)
+            LazyColumn {
+                items(userProfile) { userProfileItem ->
+                    ProfileCard(userProfileItem, onClickFunc = {
+                        // inserire il nome del composable a cui navigare
+                        navController?.navigate("user_details/${userProfileItem.userId}")
+                    })
+                }
             }
         }
     }
+}
 
-    @Composable
-    fun SetColumns(
-        mutableList: MutableList<String>,
-        buttonClick: () -> Unit,
-        mutablelStateValue: String,
-        onTextInput: (String) -> Unit
-                   ) {
-        Column(
-            verticalArrangement = Arrangement.SpaceEvenly,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            mutableList.forEach { textToDisplay ->
-                DisplayText(textToDisplay)
-            }
-            TextField(
-                value = mutablelStateValue,
-                onValueChange = onTextInput,
-                label = { Text("insert text") },
-                colors = TextFieldDefaults.textFieldColors(
-                    textColor = Color.Red,
-                    backgroundColor = Color.Blue,
-                    focusedIndicatorColor = Color.Green,
-                    unfocusedIndicatorColor = Color.Yellow
-                )
-            )
-            Button(
-                onClick = buttonClick
-            ) {
-                Text("Add new Item",
-                    modifier = Modifier.align(alignment = Alignment.CenterVertically)
-                )
-            }
-        }
+@Composable
+fun UsersApplicationNavigation(userProfile: List<UserProfile> = userProfileList) {
+    // oggetto necessario per la navigazoine
+    val navControl = rememberNavController()
+    // questo NavHost ci dice dove vogliamo navigare
+    NavHost(
+        navController = navControl,
+        // in start destination si mette il nome del composable di partenza
+        startDestination = "users_list"
+    ) {
+        composable(
+            route = "users_list",
+            content = { UserListScreen(userProfileList, navControl) })
+        composable(
+            route = "user_details/{userId}", // inseriamo tutto il path, che conterrà anche il userId
+            arguments = listOf(navArgument("userId",
+                builder = {
+                    // associamo il builder all'argomento userId
+                    type = NavType.IntType
+                } ) ),
+            content = {
+                navBackStackEntry ->  UserDetailsScreen(navBackStackEntry.arguments!!.getInt("userId"))
+            })
     }
+}
 
-
-    @Composable
-    fun DisplayText(TextToDisplay: String) {
-        Text(TextToDisplay,
-            color = Color.Yellow,
-            style = MaterialTheme.typography.h4)
-    }
-
-    @Composable
-    fun HorizontalColoredBar(colorSurf: Color = Color.Black, colorText: Color = Color.Blue) {
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+@Composable
+fun UserDetailsScreen(userProfileId: Int) {
+    val userProfile = userProfileList.first { it.userId == userProfileId }
+    Scaffold(topBar = { AppBar() }) {
         Surface(
-            modifier = Modifier.wrapContentSize(
-                //align = Alignment.CenterEnd
-            ), // prende solo lo spazio di cui ha bisogno
-            color = colorSurf
+            color = Color.White,
+            modifier = Modifier.fillMaxSize()
         )
         {
-            Text(
-                text = "Text",
-                color = colorText,
-                style = MaterialTheme.typography.h4
+            Column(
+                // modifier = androidx.compose.ui.Modifier.fillMaxWidth(),
+                //HorizontalAlignment = androidx.compose.ui.Alignment.CenterVertically,
+                //horizontalArrangement = androidx.compose.foundation.layout.Arrangement.Start
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Top
+            ) {
+                ProfilePicture(userProfile.drawableUrl, userProfile.status, imageSize = 250)
+                ProfileContent(userProfile.name, userProfile.status, Alignment.CenterHorizontally)
+            }
+        }
+    }
+}
+
+@Composable
+fun AppBar() {
+    TopAppBar(
+        navigationIcon = {
+            Icon(
+                Icons.Default.Person,
+                "Home",
+                modifier = Modifier.padding(12.dp)
+            )
+        },
+        title = { Text("Messaging Application Users") }
+    )
+
+}
+
+@Composable
+fun ProfileCard(userProfile: UserProfile, onClickFunc: () -> Unit) {
+    Card(
+        modifier = Modifier
+            .padding(
+                top = 10.dp,
+                start = 10.dp,
+                bottom = 10.dp,
+                end = 10.dp
+            )
+            .fillMaxWidth()
+            // con wrapContentHeight prendiamo tutto lo spazio a disposizione
+            .wrapContentHeight(align = Alignment.Top)
+            .clickable(onClickLabel = "go to details", onClick = onClickFunc),
+        elevation = 8.dp
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start
+        ) {
+            ProfilePicture(userProfile.drawableUrl, userProfile.status)
+            ProfileContent(userProfile.name, userProfile.status)
+        }
+    }
+}
+
+@Composable
+fun ProfilePicture(drawableUrl: String, status: Boolean, imageSize: Int = 90) {
+    Card(
+        shape = CircleShape,
+        border = BorderStroke(
+            width = 2.dp,
+            color = if (status)
+                MaterialTheme.colors.lightGreen
+            else
+                Color.Red
+        ),
+        modifier = Modifier.padding(16.dp),
+        elevation = 4.dp
+
+    ) {
+        AsyncImage(
+            model = ImageRequest.Builder(LocalContext.current)
+                .data(drawableUrl)
+                .crossfade(true)
+                .build(),
+            placeholder = painterResource(R.drawable.error),
+            contentDescription = "image",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.clip(CircleShape)
+                .size(imageSize.dp)
+        )
+    }
+}
+
+@Composable
+fun ProfileContent(name: String, status: Boolean, alignment: Alignment.Horizontal = Alignment.Start) {
+    Column(
+        modifier = Modifier
+//   la commentiamo per evitare che sia mal allineato lo screen UserProfile
+//                .fillMaxWidth()
+            .padding(8.dp),
+        horizontalAlignment = alignment
+    ) {
+        Text(
+            text = name,
+            color = Color.DarkGray,
+            style = MaterialTheme.typography.h4,
+            //   textAlign = TextAlign.Center
+        )
+        Text(
+            text = if (status)
+                "Active now"
+            else
+                "Offline",
+            color = Color.DarkGray,
+            style = MaterialTheme.typography.h6,
+            //textAlign = TextAlign.Center
+        )
+    }
+}
+
+
+/*
+@Composable
+fun MainScreen(viewModel: MainViewModel = MainViewModel()) {
+    val mutableList = viewModel.mutableLiveDataList.observeAsState(mutableListOf<String>(""))
+    val mutablelStateValue = viewModel.textField.observeAsState("")
+    Surface(color = Color.DarkGray,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        SetColumns( mutableList.value,
+            buttonClick = { viewModel.addValueToList(mutablelStateValue.value)
+                viewModel.onTextChanged("")
+                 },
+            mutablelStateValue = mutablelStateValue.value
+        ) { // definizione della lambda di quando inseriamo testo
+                input: String -> viewModel.onTextChanged(input)
+        }
+    }
+}
+
+@Composable
+fun SetColumns(
+    mutableList: MutableList<String>,
+    buttonClick: () -> Unit,
+    mutablelStateValue: String,
+    onTextInput: (String) -> Unit
+               ) {
+    Column(
+        verticalArrangement = Arrangement.SpaceEvenly,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        mutableList.forEach { textToDisplay ->
+            DisplayText(textToDisplay)
+        }
+        TextField(
+            value = mutablelStateValue,
+            onValueChange = onTextInput,
+            label = { Text("insert text") },
+            colors = TextFieldDefaults.textFieldColors(
+                textColor = Color.Red,
+                backgroundColor = Color.Blue,
+                focusedIndicatorColor = Color.Green,
+                unfocusedIndicatorColor = Color.Yellow
+            )
+        )
+        Button(
+            onClick = buttonClick
+        ) {
+            Text("Add new Item",
+                modifier = Modifier.align(alignment = Alignment.CenterVertically)
             )
         }
     }
+}
 
 
-    @Composable
-    fun GreetingText(name: String) {
-        Text(text = "Hello $name Sbrabazz!",
-            modifier = Modifier
-          //      .fillMaxSize() // occupa tutto lo schermo
-                .width(80.dp)
-                .height(200.dp)
-                .clickable( onClickLabel = "clikkami ", onClick =  {  })
-                .padding(start = 24.dp, top = 10.dp),
-            style = MaterialTheme.typography.button,
-            fontWeight = FontWeight.Bold
+@Composable
+fun DisplayText(TextToDisplay: String) {
+    Text(TextToDisplay,
+        color = Color.Yellow,
+        style = MaterialTheme.typography.h4)
+}
+
+@Composable
+fun HorizontalColoredBar(colorSurf: Color = Color.Black, colorText: Color = Color.Blue) {
+    Surface(
+        modifier = Modifier.wrapContentSize(
+            //align = Alignment.CenterEnd
+        ), // prende solo lo spazio di cui ha bisogno
+        color = colorSurf
+    )
+    {
+        Text(
+            text = "Text",
+            color = colorText,
+            style = MaterialTheme.typography.h4
         )
     }
+}
 
-    @Composable
-    fun GreetingText2(name: String) {
-        Text(text = "Hello $name Sbrabazz!",
-            modifier = Modifier
-                //      .fillMaxSize() // occupa tutto lo schermo
-                .width(80.dp)
-                .height(200.dp)
-                .clickable( onClickLabel = "clikkami ", onClick =  {  })
-                .padding(start = 24.dp, top = 10.dp),
-            style = MaterialTheme.typography.h3,
-            fontWeight = FontWeight.Bold,
-            color = Color.Red
-        )
-    }
 
-    @Composable
-    fun GreetingButton() {
-        Button(onClick = { },content =  {
-            GreetingText("Button")
-            GreetingText2("Another Button")
-        })
+@Composable
+fun GreetingText(name: String) {
+    Text(text = "Hello $name Sbrabazz!",
+        modifier = Modifier
+      //      .fillMaxSize() // occupa tutto lo schermo
+            .width(80.dp)
+            .height(200.dp)
+            .clickable( onClickLabel = "clikkami ", onClick =  {  })
+            .padding(start = 24.dp, top = 10.dp),
+        style = MaterialTheme.typography.button,
+        fontWeight = FontWeight.Bold
+    )
+}
+
+@Composable
+fun GreetingText2(name: String) {
+    Text(text = "Hello $name Sbrabazz!",
+        modifier = Modifier
+            //      .fillMaxSize() // occupa tutto lo schermo
+            .width(80.dp)
+            .height(200.dp)
+            .clickable( onClickLabel = "clikkami ", onClick =  {  })
+            .padding(start = 24.dp, top = 10.dp),
+        style = MaterialTheme.typography.h3,
+        fontWeight = FontWeight.Bold,
+        color = Color.Red
+    )
+}
+
+@Composable
+fun GreetingButton() {
+    Button(onClick = { },content =  {
+        GreetingText("Button")
+        GreetingText2("Another Button")
+    })
+}
+*/
+@Preview(showBackground = true)
+@Composable
+fun UserPreview() {
+    GyoTestAppTheme() {
+        UserDetailsScreen(0)
     }
-    */
-    @Preview(showBackground = true)
-    @Composable
-    fun UserPreview() {
-        GyoTestAppTheme() {
-            UserDetailsScreen()
-        }
-    }
-    @Preview(showBackground = true)
-    @Composable
-    fun ListUserPreview() {
-        GyoTestAppTheme() {
-            UserListScreen(userProfileList)
-        }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ListUserPreview() {
+    GyoTestAppTheme() {
+        UserListScreen(userProfileList)
     }
 }
--- app/src/main/java/com/example/gyotestapp/UserProfile.kt
+++ app/src/main/java/com/example/gyotestapp/UserProfile.kt
@@ -1,35 +1,43 @@
 package com.example.gyotestapp
-data class UserProfile (val name: String,
-                       val status: Boolean,
-                       val drawableUrl: String)
+data class UserProfile (
+    val userId: Int,
+    val name: String,
+    val status: Boolean,
+    val drawableUrl: String)
 
 val userProfileList = mutableListOf<UserProfile>(
     UserProfile(name = "Hisagi Shuhei",
+        userId = 0,
         status = true,
         drawableUrl = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSn4t4DTGwP3ucBI0gLh8fien2ZIovaCoPvaVK6Dwu-3Dvj3CVN-U5XduLxR0g6wxNIuiw&usqp=CAU"),
     UserProfile(name = "Grimmjow",
         status = false,
+        userId = 1,
         drawableUrl = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDz2u29s1pWZr53qkIwJHz0MZOcQyR5MJZvw&usqp=CAU"),
     UserProfile(name = "Hisagi Shuhei",
         status = true,
+        userId = 2,
         drawableUrl = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSn4t4DTGwP3ucBI0gLh8fien2ZIovaCoPvaVK6Dwu-3Dvj3CVN-U5XduLxR0g6wxNIuiw&usqp=CAU"),
     UserProfile(name = "Grimmjow",
         status = false,
+        userId = 3,
         drawableUrl = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDz2u29s1pWZr53qkIwJHz0MZOcQyR5MJZvw&usqp=CAU"),
     UserProfile(name = "Hisagi Shuhei",
         status = true,
+        userId = 4,
         drawableUrl = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSn4t4DTGwP3ucBI0gLh8fien2ZIovaCoPvaVK6Dwu-3Dvj3CVN-U5XduLxR0g6wxNIuiw&usqp=CAU"),
     UserProfile(name = "Grimmjow",
         status = false,
+        userId = 5,
         drawableUrl = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDz2u29s1pWZr53qkIwJHz0MZOcQyR5MJZvw&usqp=CAU"),
     UserProfile(name = "Hisagi Shuhei",
         status = true,
+        userId = 6,
         drawableUrl = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSn4t4DTGwP3ucBI0gLh8fien2ZIovaCoPvaVK6Dwu-3Dvj3CVN-U5XduLxR0g6wxNIuiw&usqp=CAU"),
     UserProfile(name = "Grimmjow",
         status = false,
+        userId = 7,
         drawableUrl = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDz2u29s1pWZr53qkIwJHz0MZOcQyR5MJZvw&usqp=CAU")
-
-
 
 )
 
